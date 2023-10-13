d35804196cd2b0cbb5530aa7911a2771dfb05a83
Ties Noordhuis <ties@bit-academy.nl>
2023-10-13 10:43:57 +0200

    use localstorage for persistent products

--- admin/admin.js
+++ admin/admin.js
@@ -1,21 +1,33 @@
 function getProductsFromLocalStorage() {
-    const storedProducts = localStorage.getItem('products');
+    const storedProducts = localStorage.getItem('jsonData');
     return JSON.parse(storedProducts) || [];
 }
 
 function getProductsFromJSON(callback) {
-    fetch('product.json')
-        .then(response => response.json())
-        .then(data => callback(data))
-        .catch(error => console.error('Fout bij laden van JSON-bestand: ', error));
+    fetch('/home/product.json')
+        .then((response) => response.json())
+        .then((data) => callback(data))
+        .catch((error) => console.error('Fout bij laden van JSON-bestand: ', error));
 }
 
-function displayProducts(products) {
-    const productList = document.getElementById('productList');
+function removeItem(id) {
+    const products = getProductsFromLocalStorage();
+    const filteredProducts = products.filter((item) => item.id != id);
+    localStorage.setItem('jsonData', JSON.stringify(filteredProducts));
+}
+
+function displayProducts() {
+    const products = getProductsFromLocalStorage();
+    const productList = document.getElementById('products');
     productList.innerHTML = '';
-    products.forEach(product => {
+    products.forEach((product) => {
         const listItem = document.createElement('li');
         listItem.textContent = product.name;
+        listItem.id = product.id;
+        listItem.addEventListener('click', (event) => {
+            removeItem(event.target.id);
+            displayProducts();
+        });
         productList.appendChild(listItem);
     });
 }
@@ -23,11 +35,10 @@ function displayProducts(products) {
 const localProducts = getProductsFromLocalStorage();
 
 if (localProducts.length > 0) {
-    displayProducts(localProducts);
+    displayProducts();
 } else {
-    getProductsFromJSON(products => {
-        displayProducts(products);
-
-        localStorage.setItem('products', JSON.stringify(products));
+    getProductsFromJSON((products) => {
+        localStorage.setItem('jsonData', JSON.stringify(products));
+        displayProducts();
     });
 }
--- home/home.js
+++ home/home.js
@@ -9,36 +9,34 @@ function updateCartCount() {
     cartCount.textContent = cartItemsCount;
 }
 
-document.addEventListener("DOMContentLoaded", function () {
-    const jsonData = [
-        {
-            "id": 0,
-            "name": "Hondenvoer",
-            "price": "29.99",
-            "image": "https://i5.walmartimages.com/asr/832ae97d-149a-4295-ab55-2496d9b264b8_1.67b261ea5ba6202c4ee603c6b53765d7.jpeg?odnWidth=1000&odnHeight=1000&odnBg=ffffff"
-        },
-    ];
-
-    // Plaats de JSON-gegevens in de lokale opslag
-    localStorage.setItem("jsonData", JSON.stringify(jsonData));
-    console.log("JSON data saved to local storage.");
-
-    // Roep de functie aan om de producten op de website weer te geven
-    loadProductsFromLocalStorage();
+document.addEventListener('DOMContentLoaded', () => {
+    if (localStorage.getItem('jsonData') == null) {
+        // Plaats de JSON-gegevens in de lokale opslag
+        fetch('product.json')
+            .then((res) => res.json())
+            .then((data) => {
+                localStorage.setItem('jsonData', JSON.stringify(data));
+                console.log('JSON data saved to local storage.');
+                loadProductsFromLocalStorage();
+            });
+    } else {
+        // Roep de functie aan om de producten op de website weer te geven
+        loadProductsFromLocalStorage();
+    }
 });
 
 // Nu, laad de JSON-gegevens uit de lokale opslag en toon ze op de website:
 function loadProductsFromLocalStorage() {
-    const jsonData = JSON.parse(localStorage.getItem("jsonData"));
+    const jsonData = JSON.parse(localStorage.getItem('jsonData'));
 
     if (!jsonData) {
-        console.log("JSON data not found in local storage.");
+        console.log('JSON data not found in local storage.');
         return;
     }
 
     const productsContainer = document.getElementById('products');
 
-    jsonData.forEach(function (product) {
+    jsonData.forEach((product) => {
         const productCard = document.createElement('div');
         productCard.className = 'card';
 
@@ -66,9 +64,6 @@ function loadProductsFromLocalStorage() {
         productsContainer.appendChild(productCard);
     });
 }
-
-// Roep de functie aan om de producten van de lokale opslag op de website weer te geven
-loadProductsFromLocalStorage();
 
 function getProductFromButton(button) {
     const productCard = button.closest('.card');
@@ -110,56 +105,5 @@ addToCartButtons.forEach((button) => {
         localStorage.setItem('cartItemsCount', cartItemsCount);
     });
 });
-
-const productsContainer = document.getElementById('products');
-
-fetch('product.json')
-    .then(response => response.json())
-    .then(data => {
-        const products = data;
-
-        products.forEach(function (product) {
-            const productCard = document.createElement('div');
-            productCard.className = 'card';
-
-            const imageElement = document.createElement('img');
-            imageElement.src = product.image;
-            productCard.appendChild(imageElement);
-
-            const nameElement = document.createElement('h3');
-            nameElement.textContent = product.name;
-            productCard.appendChild(nameElement);
-
-            const priceElement = document.createElement('p');
-            priceElement.textContent = 'Price: $';
-            productCard.appendChild(priceElement);
-
-            const priceValueElement = document.createElement('span');
-            priceValueElement.textContent = product.price;
-            priceElement.appendChild(priceValueElement);
-
-            const addToCartButton = document.createElement('button');
-            addToCartButton.textContent = 'Add to Cart';
-            addToCartButton.className = 'add-to-cart';
-            productCard.appendChild(addToCartButton);
-
-            productsContainer.appendChild(productCard);
-        });
-
-        const addToCartButton = document.querySelectorAll('.add-to-cart');
-
-        addToCartButton.forEach((button) => {
-            button.addEventListener('click', () => {
-                cartItemsCount++;
-                updateCartCount();
-
-                const product = getProductFromButton(button);
-                saveProductToCart(product);
-
-                localStorage.setItem('cartItemsCount', cartItemsCount);
-            });
-        });
-    })
-    .catch(error => console.log(error));
 
 updateCartCount();
