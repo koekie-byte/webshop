c8eb08a589156a70a15d189d84f1b25d02181556
koekie-byte <145991399+koekie-byte@users.noreply.github.com>
2023-12-07 08:34:15 +0100

    Add files via upload

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
-# webshop
-webshop
+## Webshop Admin Page
+Description
+This project represents an admin page for managing products in a webshop. It includes functionality for resetting product data, displaying products in a table, and allowing for editing and removal of products.
+
+Table of Contents
+Installation
+Usage
+Features
+File Structure
+Contributing
+License
+Installation
+Clone the repository to your local machine:
+
+bash
+Copy code
+git clone [git@bitlab.bit-academy.nl:eabe2084-29d7-11ed-81f7-4213e7ee7fac/b40bda54-0c0d-4815-b8cf-01886f4f5716/The-Webshop-8f618a0b-522cca8b.git]
+Open the admin.html file in your preferred web browser.
+
+Usage
+Click the "Reset" button to clear and reset the product data.
+View and manage products in the table.
+Use the "Edit" link to navigate to the edit page for a specific product.
+Click the "Remove" button to delete a product.
+Features
+Reset Button: Clears and resets the product data to the initial state.
+Product Table: Displays product information in a table format.
+Edit Link: Navigates to the edit page for a selected product.
+Remove Button: Deletes a product from the list.
+File Structure
+admin.html: Main HTML file for the admin page.
+admin.js: JavaScript file for fetching and managing product data.
+admin.css: CSS file for styling the admin page.
+Contributing
+If you would like to contribute to this project, feel free to submit a pull request.
+
+License
+This project is licensed under the MIT License.
---
+++ winkelwagen.js
@@ -0,0 +1,66 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const cartItemsContainer = document.getElementById('cart-items');
+    const totalPriceContainer = document.getElementById('total-price');
+    const cartItems = JSON.parse(localStorage.getItem('cartItems')) || [];
+    let totalPrice = 0;
+
+    const productQuantities = {};
+
+    if (cartItems.length > 0) {
+        cartItems.forEach(product => {
+            const productName = product.name;
+            const productPrice = parseFloat(product.price);
+
+            if (productQuantities.hasOwnProperty(productName)) {
+                productQuantities[productName].quantity += 1;
+                productQuantities[productName].totalPrice += productPrice;
+            } else {
+                productQuantities[productName] = {
+                    name: productName,
+                    quantity: 1,
+                    totalPrice: productPrice,
+                };
+            }
+        });
+
+        for (const productName in productQuantities) {
+            if (productQuantities.hasOwnProperty(productName)) {
+                const product = productQuantities[productName];
+
+                const productElement = document.createElement('div');
+                productElement.className = 'product-info';
+
+                const nameElement = document.createElement('p');
+                nameElement.textContent = product.name;
+                productElement.appendChild(nameElement);
+
+                const quantityElement = document.createElement('p');
+                quantityElement.textContent = `Quantity: ${product.quantity}`;
+                productElement.appendChild(quantityElement);
+
+                const priceElement = document.createElement('p');
+                const price = parseFloat(product.totalPrice / product.quantity);
+                priceElement.textContent = `Price per item: €${price.toFixed(2)}`;
+                productElement.appendChild(priceElement);
+
+                const totalPriceElement = document.createElement('p');
+                totalPriceElement.textContent = `Total price: €${product.totalPrice.toFixed(2)}`;
+                productElement.appendChild(totalPriceElement);
+
+                cartItemsContainer.appendChild(productElement);
+
+                totalPrice += product.totalPrice;
+            }
+        }
+    } else {
+        const emptyMessage = document.createElement('p');
+        emptyMessage.textContent = 'The shopping cart is empty.';
+        cartItemsContainer.appendChild(emptyMessage);
+    }
+
+    totalPriceContainer.textContent = `Total Price: €${totalPrice.toFixed(2)}`;
+});
+function clearStorageAndRefresh() {
+    localStorage.clear();
+    location.reload();
+}
